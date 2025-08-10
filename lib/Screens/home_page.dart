df5d59a1f24ab39b8cefea834ec77f14e15fc4db
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-08-10 21:38:20 +0500

    chnage color

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -10,6 +10,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
   }
 }
