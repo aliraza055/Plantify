8d2db99b46ebef2a7d80dbffbfacb8c47764bca0
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-08-10 21:38:44 +0500

    .

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -10,8 +10,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return Scaffold(      
       body: Container(
         color: Colors.blue,
       ),
